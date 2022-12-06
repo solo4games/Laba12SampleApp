747eb694614196b94657a55b675bf8facec52156
Andrey <dron1611@mail.ru>
2022-12-06 12:25:26 +0300

    Laba11 Done

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :users
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
